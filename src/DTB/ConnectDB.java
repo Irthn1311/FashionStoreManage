8536c439f048b3816886170d891ab7dff93103ac
Irthn1311 <nhuutri1311@gmail.com>
2025-04-15 21:15:33 +0700

    đổi phiên bản jdbc

--- src/DTB/ConnectDB.java
+++ src/DTB/ConnectDB.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnectDB {
     private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=FashionStore;encrypt=true;trustServerCertificate=true";
     private static final String USER = "sa";  // Tài khoản SQL Server của bạn
-    private static final String PASSWORD = "1201nhtri1311";  // Mật khẩu SQL Server của bạn
+    private static final String PASSWORD = "nhapmatkhau";  // Mật khẩu SQL Server của bạn
 
     public static Connection getConnection() {
         try {
